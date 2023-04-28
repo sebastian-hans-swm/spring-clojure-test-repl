5d5eee3ced0100dc63787c08ae967a6dcfa12a74
Sebastian Hans <Hans.Sebastian@swm.de>
2023-04-28 16:09:25 +0200

    start REPL with cider-nrepl-handler

--- src/main/java/de/sebhans/test/repl/REPLServer.java
+++ src/main/java/de/sebhans/test/repl/REPLServer.java
@@ -10,44 +10,49 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
 public class REPLServer implements ApplicationContextAware, InitializingBean, DisposableBean {
-	private static final Logger LOGGER = LoggerFactory.getLogger(REPLServer.class);
-	private static ApplicationContext applicationContext = null;
+    private static final Logger LOGGER = LoggerFactory.getLogger(REPLServer.class);
+    private static ApplicationContext applicationContext = null;
 
-	public static ApplicationContext getContext() {
-		return applicationContext;
-	}
+    public static ApplicationContext getContext() {
+        return applicationContext;
+    }
 
-	private final long port;
-	private final String bindAddress;
-	private Object serverInstance = null;
+    private final long port;
+    private final String bindAddress;
+    private Object serverInstance = null;
 
-	public REPLServer(final String bindAddress, final long port) {
-		this.port = port;
-		this.bindAddress = bindAddress;
-	}
+    public REPLServer(final String bindAddress, final long port) {
+        this.port = port;
+        this.bindAddress = bindAddress;
+    }
 
-	@Override
-	public void setApplicationContext(final ApplicationContext applicationContext) throws BeansException {
-		REPLServer.applicationContext = applicationContext;
-	}
+    @Override
+    public void setApplicationContext(final ApplicationContext applicationContext) throws BeansException {
+        REPLServer.applicationContext = applicationContext;
+    }
 
-	@Override
-	public void afterPropertiesSet() {
-		if (serverInstance == null) {
-			Clojure.var("clojure.core", "require").invoke(Clojure.read("nrepl.server"));
-			serverInstance = Clojure.var("nrepl.server", "start-server")
-					.invoke(Clojure.read(":port"), port,
-							Clojure.read(":bind"), bindAddress);
-			LOGGER.info("Started nREPL on {}:{}", bindAddress, port);
-		}
-	}
+    @Override
+    public void afterPropertiesSet() {
+        if (serverInstance == null) {
+            Clojure.var("clojure.core", "require").invoke(Clojure.read("nrepl.server"));
+            Clojure.var("clojure.core", "require")
+                    .invoke(Clojure.read("cider.nrepl"),
+                            Clojure.read(":refer"),
+                            Clojure.read("(cider-nrepl-handler)"));
+            serverInstance = Clojure.var("nrepl.server", "start-server")
+                    .invoke(Clojure.read(":port"), port,
+                            Clojure.read(":bind"), bindAddress,
+                            Clojure.read(":handler"), Clojure.var("cider-nrepl-handler"));
+            LOGGER.info("Started nREPL on {}:{}", bindAddress, port);
+        }
+    }
 
-	@Override
-	public void destroy() {
-		if (serverInstance != null) {
-			LOGGER.info("Shutting down nREPL on {}:{}", bindAddress, port);
-			Clojure.var("nrepl.server", "stop-server").invoke(serverInstance);
-			serverInstance = null;
-		}
-	}
+    @Override
+    public void destroy() {
+        if (serverInstance != null) {
+            LOGGER.info("Shutting down nREPL on {}:{}", bindAddress, port);
+            Clojure.var("nrepl.server", "stop-server").invoke(serverInstance);
+            serverInstance = null;
+        }
+    }
 }
